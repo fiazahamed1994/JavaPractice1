2d5a73a26fc6e6678f5173b0359b8fcfd4417f3c
My <My@DESKTOP-LSSI4SD>
2024-04-30 02:21:59 -0700

    Pushing code

---
+++ src/com/practice/Task.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Task {
+	public void method1() {
+    System.out.println("one");
+	}
+	
+	public static void main(String[] args) {
+		Task t = new Task();
+		t.method1();
+	}
+	
+}
+
